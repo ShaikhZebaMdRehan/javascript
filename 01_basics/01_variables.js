92944e9bc2120e16b888de60e5cca850b611843f
ShaikhZebaMdRehan <shaikhzeba3221@gmail.com>
2025-05-02 21:19:05 +0530

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -5,6 +5,8 @@ accountCity = "Jaipur";
 
 /* accountId = 2; */ // Value will not change br=ecause of const variable and it will give error
 
+console.log(accountId);
+
 /* Prefer not to use var variable because of issue in block scope and functional scope */
 
 /* Change the value of all these variable it will change the value */
@@ -12,8 +14,6 @@ accountEmail = "johndoe@gmail.com";
 accountPassword = "5";
 accountCity = "Bengaluru";
 let accountState; // Now add one more variable name state nut state will change that;s why we use let variable ( accountState will print undefined because we not assign a value of state ) 
-
-console.log(accountId);
 
 /* print all the outputs in one command and output will come in TABULAR FORM */
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
