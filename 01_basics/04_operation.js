d6e4507107c73758060b6c0aba19d0c6be16c901
ShaikhZebaMdRehan <shaikhzeba3221@gmail.com>
2025-05-13 15:10:22 +0530

    operation

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,61 @@
+let value = 5;
+let negValue = -value;
+console.log(negValue);
+
+/* Addition (+)
+Subtraction (-)
+Multiplication (*)
+Division (/)
+Remainder (%) ( It is also called Modulus )
+Exponention (**) */
+
+console.log(2+2);
+console.log(10-5);
+console.log(2*3);
+console.log(2/5);
+console.log(2%3);
+console.log(2**2);
+
+/* Add String */
+let str1 = "Shaikh";
+let str2 = " Zeba";
+let str3 = str1 + str2;
+console.log(str3);
+
+/* Complex Oprations but it is not an good practice of coding */
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+// console.log((3 + 4) * 5 % 3);
+
+console.log(true);
+console.log(+true);
+// console.log(true+); If we put a '+' sign after 'true' or something, it will give an error 
+console.log(+""); // If it is empty, it will give the output 0
+
+// It is not a good practice of code it reduce the code readability
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+console.log(num1, num2, num3);
+
+// Postfix & Prefix
+
+let gameCounter = 100;
+gameCounter++; //Postfix
+++gameCounter; //Prefix
+console.log(gameCounter);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
