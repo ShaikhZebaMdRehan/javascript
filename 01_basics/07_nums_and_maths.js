cc8b31194988f56488d1790c1e40e9b497ad2b4b
ShaikhZebaMdRehan <shaikhzeba3221@gmail.com>
2025-05-26 13:59:51 +0530

    nums_and_maths

--- 01_basics/07_nums_and_maths.js
+++ 01_basics/07_nums_and_maths.js
@@ -52,7 +52,7 @@ console.log(Math.max(4, 9, 5, 7, 3, 8)); // It will give maximum value
 
 // Random
 console.log(Math.random()); // A random value always comes between 0 to 1
-console.log((Math.random()*10) + 1);
+console.log((Math.random()*10) + 1); // +1 means it removes the 0
 console.log(Math.floor(Math.random()*10) +1);
 
 const min = 10;
